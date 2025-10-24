ca2eb7df4b326e763ea8d1f00792b372b694ae7d
Victor <victor.lam@pinkkamii.com>
2025-10-23 20:16:09 -0700

    Initial Commit

---
+++ ScreenGrabberTests/ScreenGrabberTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenGrabberTests.swift
+//  ScreenGrabberTests
+//
+//  Created by Victor Lam on 10/23/25.
+//
+
+import Testing
+@testable import ScreenGrabber
+
+struct ScreenGrabberTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
